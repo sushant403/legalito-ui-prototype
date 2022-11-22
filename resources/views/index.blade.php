b2f6839377ea65576e2fa66d3675e9150e81e767
Sushant Poudel <sushantpaudel@gmail.com>
2022-11-22 15:02:10 +0545

    homepage package rendered

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,8 @@
             <div class="col-lg-10 order-lg-2">
                 <a href="{{ route('product') }}" class="d-inline-block">
                     <figure class="img-overlay mb-0">
-                        <img src="{{ asset('img/home-legalito_2.png') }}" alt="Legalito" class="img-thumbnail" width="auto">
+                        <img src="{{ asset('img/home-legalito_2.png') }}" alt="Legalito" class="img-thumbnail"
+                            width="auto">
                         <div class="img-hover">
                             <p class="img-text">View Product</p>
                         </div>
@@ -38,7 +39,8 @@
     <div class="bg-black text-light text-center">
         <div class="container py-5 py-md-5">
             <p class="lead mb-4 legalito-gold">Smart people - Smart technology</p>
-            <h1 class="col-lg-8 mx-auto" style="line-height: 3rem; font-weight:400">We've over 15 years' experience building software that makes the work of property lawyers less stressful.</h1>
+            <h1 class="col-lg-8 mx-auto" style="line-height: 3rem; font-weight:400">We've over 15 years' experience building
+                software that makes the work of property lawyers less stressful.</h1>
         </div>
     </div>
 
@@ -47,95 +49,125 @@
             <h4 class="text-center mb-5">Designed from years of experience</h4>
             <div class="row gy-4">
                 <div class="col-md">
-                    <div class="card text-center pb-4">
-                        <div class="card-header bg-light border-bottom-0 py-4">
-                            <small class="lead legalito-gold pt-4" style="font-size: 0.75rem">Streamlining pre-contract enquiries</small>
-                            <h3 class="fw-normal my-4 text-dark">Smart Enquiries</h3>
-                            <span class="text-dark">Removes the guesswork and stress from enquiry handling.</span>
-                        </div>
-                        <div class="card-body">
-                            <ul class="list-unstyled text-muted">
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Relevant Enquiry</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Enquiries relevant to property types</span>
-                                </li>
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Smart Response</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Automated intelligent answers</span>
-                                </li>
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Personal knowledge base</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Use your own enquiries</span>
-                                </li>
-                            </ul>
-                            <a href="{{ route('product') }}" class="legalito-button-outline mb-3" style="padding: 16px 110px">Learn More</a>
-                            <br><br>
-                            <a href="{{ route('product') }}" class="legalito-button" style="padding: 16px 80px">Try it now - for free</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card text-center pb-4">
-                        <div class="card-header bg-light border-bottom-0 py-4">
-                            <small class="lead legalito-gold pt-4" style="font-size: 0.75rem">Streamlining pre-contract enquiries</small>
-                            <h3 class="fw-normal my-4 text-dark">Smart Enquiries</h3>
-                            <span class="text-dark">Removes the guesswork and stress from enquiry handling.</span>
-                        </div>
-                        <div class="card-body">
-                            <ul class="list-unstyled text-muted">
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Relevant Enquiry</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Enquiries relevant to property types</span>
-                                </li>
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Smart Response</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Automated intelligent answers</span>
-                                </li>
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Personal knowledge base</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Use your own enquiries</span>
-                                </li>
-                            </ul>
-                            <a href="{{ route('product') }}" class="legalito-button-outline mb-3" style="padding: 16px 110px">Learn More</a>
-                            <br><br>
-                            <a href="{{ route('product') }}" class="legalito-button" style="padding: 16px 80px">Try it now - for free</a>
+                    <div class="card border-0 has-bg-img bg-img-legalito text-center" style="font-size:smaller">
+                        <div class="card-img-overlay text-light">
+                            <div class="card-header border-bottom-0 py-4">
+                                <small class="lead legalito-gold pt-5" style="font-size: 0.75rem">Streamlining pre-contract
+                                    enquiries</small>
+                                <h3 class="fw-normal py-2 mb-2 text-light">Smart Enquiries</h3>
+                                <span class="text-light">Removes the guesswork and stress from enquiry
+                                    handling.</span>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-unstyled text-muted">
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Relevant Enquiry</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Enquiries relevant to property
+                                            types</span>
+                                    </li>
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Smart Response</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Automated intelligent
+                                            answers</span>
+                                    </li>
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Personal knowledge base</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Use your own enquiries</span>
+                                    </li>
+                                </ul>
+                                <div class="pb-4">
+                                    <a href="{{ route('product') }}" class="legalito-button-outline mb-3"
+                                        style="padding: 16px 110px">Learn More</a>
+                                    <br><br><br>
+                                    <a href="{{ route('product') }}" class="legalito-button" style="padding: 16px 80px">Try it
+                                        now - for free</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-md">
-                    <div class="card text-center pb-4">
-                        <div class="card-header bg-light border-bottom-0 py-4">
-                            <small class="lead legalito-gold pt-4" style="font-size: 0.75rem">Streamlining pre-contract enquiries</small>
-                            <h3 class="fw-normal my-4 text-dark">Smart Enquiries</h3>
-                            <span class="text-dark">Removes the guesswork and stress from enquiry handling.</span>
+                    <div class="card border-0 has-bg-img bg-img-legalito text-center" style="font-size:smaller">
+                        <div class="card-img-overlay text-light">
+                            <div class="card-header border-bottom-0 py-4">
+                                <small class="lead legalito-gold pt-5" style="font-size: 0.75rem">Streamlining pre-contract
+                                    enquiries</small>
+                                <h3 class="fw-normal py-2 mb-2 text-light">Smart Enquiries</h3>
+                                <span class="text-light">Removes the guesswork and stress from enquiry
+                                    handling.</span>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-unstyled text-muted">
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Relevant Enquiry</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Enquiries relevant to property
+                                            types</span>
+                                    </li>
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Smart Response</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Automated intelligent
+                                            answers</span>
+                                    </li>
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Personal knowledge base</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Use your own enquiries</span>
+                                    </li>
+                                </ul>
+                                <div class="pb-4">
+                                    <a href="{{ route('product') }}" class="legalito-button-outline mb-3"
+                                        style="padding: 16px 110px">Learn More</a>
+                                    <br><br><br>
+                                    <a href="{{ route('product') }}" class="legalito-button" style="padding: 16px 80px">Try it
+                                        now - for free</a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <ul class="list-unstyled text-muted">
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Relevant Enquiry</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Enquiries relevant to property types</span>
-                                </li>
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Smart Response</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Automated intelligent answers</span>
-                                </li>
-                                <li class="py-2" style="text-align: left">
-                                    <i class="material-icons ms-2" aria-hidden="true">check</i>
-                                    <strong>Personal knowledge base</strong> <br>
-                                    <span style="font-size: smaller;margin-left:1.8rem">Use your own enquiries</span>
-                                </li>
-                            </ul>
-                            <a href="{{ route('product') }}" class="legalito-button-outline mb-3" style="padding: 16px 110px">Learn More</a>
-                            <br><br>
-                            <a href="{{ route('product') }}" class="legalito-button" style="padding: 16px 80px">Try it now - for free</a>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card border-0 has-bg-img bg-img-legalito text-center" style="font-size:smaller">
+                        <div class="card-img-overlay text-light">
+                            <div class="card-header border-bottom-0 py-4">
+                                <small class="lead legalito-gold pt-5" style="font-size: 0.75rem">Streamlining pre-contract
+                                    enquiries</small>
+                                <h3 class="fw-normal py-2 mb-2 text-light">Smart Enquiries</h3>
+                                <span class="text-light">Removes the guesswork and stress from enquiry
+                                    handling.</span>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-unstyled text-muted">
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Relevant Enquiry</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Enquiries relevant to property
+                                            types</span>
+                                    </li>
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Smart Response</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Automated intelligent
+                                            answers</span>
+                                    </li>
+                                    <li class="py-2 text-light" style="text-align: left">
+                                        <i class="material-icons ms-2" aria-hidden="true">check</i>
+                                        <strong>Personal knowledge base</strong> <br>
+                                        <span style="font-size: smaller;margin-left:1.8rem">Use your own enquiries</span>
+                                    </li>
+                                </ul>
+                                <div class="pb-4">
+                                    <a href="{{ route('product') }}" class="legalito-button-outline mb-3"
+                                        style="padding: 16px 110px">Learn More</a>
+                                    <br><br><br>
+                                    <a href="{{ route('product') }}" class="legalito-button" style="padding: 16px 80px">Try it
+                                        now - for free</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
